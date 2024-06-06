7f8659a991b21289d586199842a4746e13f0ab4f
s24675 <s24675@pjwstk.edu.pl>
2024-06-06 19:07:31 +0200

    Stworzono strukturę , dodano modele , AppDbContext.cs , skonfigurowano Program.cs z Connection Stringiem

---
+++ APBD8/Context/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using APBD8.Models;
+using EfCodeFirst.EfConfigurations;
+using EfCodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCodeFirst.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Doctor> Students { get; set; }
+    public DbSet<Medicament> Groups { get; set; }
+    public DbSet<Patient> StudentGroups { get; set; }
+    public DbSet<Prescription> StudentGroups { get; set; }
+    public DbSet<Prescription_Medicament> StudentGroups { get; set; }
+
+
+    public AppDbContext() {}
+
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        /*
+        modelBuilder.Entity<Student>(s =>
+        {
+            s.Property()
+        });
+        */
+
+        /*
+        modelBuilder.ApplyConfigurationsFromAssembly();
+        */
+        
+    }
+}
